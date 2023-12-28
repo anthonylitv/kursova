62e5bd7f7b29109741c513f7ecf6eb3d8452198f
antonfront <toxalitvinenko@gmail.com>
2023-12-28 16:15:29 +0200

    gii changes

--- config/web.php
+++ config/web.php
@@ -15,6 +15,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'VhEye1fW3z9RSx1DbXGwL-O-TE7_iBc0',
+            'baseUrl' => ''
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -42,14 +43,21 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+        
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-            'rules' => [
-            ],
+            'rules' => array(
+                '' => 'site/index',
+                '<action>' => 'site/<action>',
+            ),
         ],
-        */
+        
+    ],
+    'modules' => [
+        'admin' => [
+            'class' => 'app\modules\admin\Module',
+        ],
     ],
     'params' => $params,
 ];
